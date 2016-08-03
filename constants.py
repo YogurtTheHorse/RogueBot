982f9b96d688b51744c41b219968354d2b8370d6
yegorf1 <yegorf1@gmail.com>
2016-08-03 16:32:27 +0300

    Added constants

---
+++ constants.py
@@ -0,0 +1,13 @@
+BUDDHA = 'Будда'
+BUDDHA_NUM = 0
+JESUS = 'Иисус'
+JESUS_NUM = 1
+ALLAH = 'Аллах'
+ALLAH_NUM = 2
+AUTHOR = 'Рассказчик'
+AUTHOR_NUM = 3
+
+SMART_LEVEL = 10
+DUMMY_LEVEL = -SMART_LEVEL
+
+GOD_LEVEL = 3

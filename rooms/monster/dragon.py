2bb7625e0c3f30c7b72550fc67b8cb5df33daa02
yegorf1 <yegorf1@gmail.com>
2016-08-27 22:27:56 +0300

    Made dragon more powerful

--- rooms/monster/dragon.py
+++ rooms/monster/dragon.py
@@ -1,6 +1,6 @@
 name = 'Дракон'
 
-hp = 70
+hp = 100
 damage_range =  ( 15, 30 )
 
 coins = 300
@@ -9,3 +9,7 @@ loot = [ 'dragon_sword' ]
 
 def enter(user, reply):
 	reply('Большой и красный, как в сказке')
+
+	if user.story_level < 1:
+		reply('Он не заметил тебя прошел мимо. Мелкий еще.')
+		user.leave(reply)

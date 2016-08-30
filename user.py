f9cc97263b1edb00ccda09449e3c2c816a8aada4
yegorf1 <yegorf1@gmail.com>
2016-08-30 23:39:10 +0300

    Removed imagination

--- user.py
+++ user.py
@@ -275,8 +275,7 @@ class User(object):
 	def get_fight_actions(self):
 		actions = [
 			KICK_ARM,
-			KICK_MAGIC,
-			USE + 'Воображение'
+			KICK_MAGI# USE + 'Воображение'
 		]
 
 		for i in self.get_items():

76be1d5ff804757b29cfc447fb36942ed02670fa
yegorf1 <yegorf1@gmail.com>
2016-09-02 23:19:11 +0300

    Some defence for knight

--- rooms/monster/knight.py
+++ rooms/monster/knight.py
@@ -13,5 +13,14 @@ def enter(user, reply):
 	reply('Весь в доспехах')
 
 	if user.story_level < 3:
-		reply('Я не бью маленьких деочек типа тебя')
+		reply('Я не бью маленьких девочек типа тебя')
 		user.leave(reply)
+
+def make_damage(user, reply, dmg):
+	hp = user.get_room_temp('hp', 0)
+	hp -= max(0, dmg - 50)
+
+	if hp <= 0:
+		user.won(reply)
+	else:
+		user.set_room_temp('hp', hp)

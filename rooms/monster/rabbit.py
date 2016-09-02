ae83be13a7c328e25905d0bbc86b62e068adf5b6
yegorf1 <yegorf1@gmail.com>
2016-09-02 22:43:01 +0300

    Added rabbit

---
+++ rooms/monster/rabbit.py
@@ -0,0 +1,14 @@
+from constants import *
+
+name = 'Кролик'
+
+hp = 700
+element = NONE
+damage_range = ( 1, 3 )
+
+coins = 3
+
+loot = [ ]
+
+def enter(user, reply):
+	reply('Его зубы в крови своих жертв')

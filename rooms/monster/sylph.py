c85da45382e96080ee166445ccf50c7306d4ba6e
Dmitriy Varenko <Dm.varenkov@gmail.com>
2016-09-02 10:15:34 +0300

    add sylph and ballet_tuty

---
+++ rooms/monster/sylph.py
@@ -0,0 +1,12 @@
+
+name = 'Сильфида'
+hp = 25
+damage_range = (7, 14)
+
+coins = 20
+
+loot = ['ballet_tutu']  # Балетная пачка
+
+
+def enter(user, reply):
+	reply('Не доверяй этой женшине, ой не доверяй!')

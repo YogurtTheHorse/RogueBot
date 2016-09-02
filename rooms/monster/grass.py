a79c4b74fe842ee9ffc9131e2d375075e36a73a8
yegorf1 <yegorf1@gmail.com>
2016-09-02 22:45:52 +0300

    Added grass

---
+++ rooms/monster/grass.py
@@ -0,0 +1,28 @@
+from constants import *
+
+name = 'Трава'
+
+hp = 12
+element = WATER
+damage_range =  ( 0, 0 )
+
+coins = 1
+
+loot = [ ]
+
+def enter(user, reply):
+	msg = (
+		'Это трава в горшке'
+	)
+	reply(msg)
+
+def get_actions(user):
+	return user.get_fight_actions() + [ 'Уйти' ]
+
+def action(user, reply, text):
+	if text == 'Уйти':
+		reply('Ты просто ушел от травы')
+
+		user.leave(reply)
+	else:
+		user.fight_action(reply, text)

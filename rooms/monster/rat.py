1a76157f8da62a6d94cb13165318a82e130fc115
yegorf1 <yegorf1@gmail.com>
2016-09-02 21:13:57 +0300

    Rat fixed

--- rooms/monster/rat.py
+++ rooms/monster/rat.py
@@ -11,7 +11,7 @@ coins = 7
 loot = [ 'rat_tooth' ]
 
 def enter(user, reply):
-	if user.has_aura(BUDDHA_AURA):
+	if user.has_aura(AURA_BUDDHA):
 		reply('Крысы не трогают буддистов. Иди с миром')
 		user.won(reply)
 	else:

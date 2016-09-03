a62db10f7f62af7b034a370e388dcd4e4fd49852
yegorf1 <yegorf1@gmail.com>
2016-09-03 21:49:17 +0300

    Chubaka added

---
+++ rooms/monster/chubaka.py
@@ -0,0 +1,16 @@
+name = 'Длиноволоссая брюнетка'
+
+hp = 120
+element = NONE
+damage_range =  ( 16, 18 )
+
+coins = 25
+
+loot = [ 'laser_gun' ]
+
+def enter(user, reply):
+	reply('Ан, нет. Это чубака')
+
+	if user.has_aura(AURA_BUDDHA):
+		reply('Чубака уважает буддистов, правда не знает, кто это, но уважает.')
+		user.leave(reply)

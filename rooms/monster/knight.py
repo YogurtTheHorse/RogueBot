dcfa67a45aec2cd74a1fdd9450330c764ac2348e
yegorf1 <yegorf1@gmail.com>
2016-09-03 14:42:25 +0300

    Knight changed

--- rooms/monster/knight.py
+++ rooms/monster/knight.py
@@ -18,7 +18,7 @@ def enter(user, reply):
 
 def make_damage(user, reply, dmg):
 	hp = user.get_room_temp('hp', 0)
-	hp -= max(0, dmg - 50)
+	hp -= max(0, dmg - 10)
 
 	if hp <= 0:
 		user.won(reply)

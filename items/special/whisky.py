29450a2dd5efaeadc5c129e26cfb27b66f47518a
yegorf1 <yegorf1@gmail.com>
2016-09-04 19:08:22 +0300

    Whisky typo

--- items/special/whisky.py
+++ items/special/whisky.py
@@ -4,7 +4,7 @@ price = 100
 
 def on_room(user, reply, room):
 	if room.room_type == 'monster' and room.hp > 120:
-		reply('Ну нахрен, ты это вообще видел? Я сваливаю. И виски забиру с собой!')
+		reply('Ну нахрен, ты это вообще видел? Я сваливаю. И виски заберу с собой!')
 		user.remove_item('whisky')
 	else:
 		reply('Допивать будете?')

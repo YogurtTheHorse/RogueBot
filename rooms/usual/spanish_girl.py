57e61191ba5788580902aa199bf5b8e653ec5b95
yegorf1 <yegorf1@gmail.com>
2016-09-05 15:20:04 +0300

    Added Spanish Girl

---
+++ rooms/usual/spanish_girl.py
@@ -0,0 +1,54 @@
+READY = 'Подойти ближе'
+ESCAPE = 'Уйти'
+CLOSER = 'Подойти еще ближе'
+
+name = 'Красотка'
+
+def enter(user, reply):
+	msg = ( 
+		'Ты видишь огненную красотку\n'
+		'Она улыбнулась тебе и поманила пальчиком к себе'
+	)
+	reply(msg)
+	user.set_room_temp('question', 'first')
+
+def action(user, reply, text):
+	question = user.get_room_temp('question', def_val='first')
+
+	if question == 'first':
+		if text  == READY:
+			reply('Вы подходите ближе, а красотка начинает раздеваться')
+			user.set_room_temp('question', 'undress')
+		else:
+			reply('Красотка обиженно на вас смотрит')
+			user.leave(reply)
+	elif question == 'undress':
+		if text == CLOSER:
+			if user.has_item('christ'):
+				reply('Красотка исчезает, а на ее месте пояляется Винцо')
+				user.add_item('special', 'wine')
+				user.leave(reply)
+			else:
+				msg = (
+					'ВНЕЗАПНО красотка исчезает.\n'
+					'Вместо нее появляются три мужика в красном\n'
+					'..\n'
+					'НИКТО НЕ ОЖИДАЕТ ИСПАНСКУЮ ИНКВИЗИЦИЮ!'
+					'На костёр неверного!'
+				)
+				reply(msg)
+				user.death(reply, reason='Инквизиция')
+		else:
+			reply('Красотка обиженно на вас смотрит')
+			user.leave(reply)
+
+def get_actions(user):
+	question = user.get_room_temp('question', def_val='first')
+	ans = [ ]
+
+	if question == 'first':
+		ans = [ READY, ESCAPE ]
+	else:
+		ans = [ CLOSER, ESCAPE]
+
+	return ans

fefcb2b5fc866c0c7df1084d1de53287b1cd933c
yegorf1 <yegorf1@gmail.com>
2016-09-05 11:13:28 +0300

    hotfix!

--- user.py
+++ user.py
@@ -408,7 +408,7 @@ class User(object):
 			if item:
 				dmg = item.fight_use(self, reply, room)
 				if dmg != 0:
-					dmg += self.get_damage(reply) + self.get_damage_bonus(reply)
+					dmg += self.get_damage() + self.get_damage_bonus(reply)
 
 				if item.disposable:
 					self.remove_item(item.code_name)

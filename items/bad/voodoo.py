fa93700fb325bd513495f5df077014cb63703cc9
Teogor <sarsncs@gmail.com>
2016-09-06 15:21:38 +0300

    Update voodoo.py

--- items/bad/voodoo.py
+++ items/bad/voodoo.py
@@ -1,11 +1,11 @@
 
-name = 'кукла вуду'
+name = 'Кукла вуду'
 
-description = ('небольшая тряпичная кукла, говорят, в умелых руках она творит чудеса')
+description = 'Небольшая тряпичная кукла, говорят, в умелых руках она творит чудеса'
 
 price = 50
 
 def on_room(user, reply, room):	
-	reply('У вас закололо в груди')
-	user.make_damage(20,30, reply, death=false)
-	user.defence -= 5
+	reply('У тебя закололо в груди')
+	user.make_damage(20,30, reply, death=False)
+	

806cc8885b328a4d0c88c6bf4c87471a7724ed5d
yegorf1 <yegorf1@gmail.com>
2016-09-06 14:14:17 +0300

    Some fixes

--- items/good/checkpoint.py
+++ items/good/checkpoint.py
@@ -7,8 +7,8 @@ usable = True
 def on_use(user, reply):
 	user.remove_item('checkpoint')
 
-	if user.ger_variable('was_checkpoint', def_val=False):
-		user.ser_variable('was_checkpoint', True)
+	if user.get_variable('was_checkpoint', def_val=False):
+		user.set_variable('was_checkpoint', True)
 		user.save()
 		reply('Вы спасены и сохранены!')
 	else:

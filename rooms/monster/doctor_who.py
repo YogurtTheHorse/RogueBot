dfd04dac4e14c205c30e27ae062e621d824126e2
TheKiba <agrekov@aforex.ru>
2016-09-06 13:43:13 +0200

    Counter of doctor_num fixes

--- rooms/monster/doctor_who.py
+++ rooms/monster/doctor_who.py
@@ -19,6 +19,17 @@ def enter(user, reply):
 def get_actions(user):
 	return user.get_fight_actions() + [ 'Сдаться' ]
 
+def make_damage(user, reply, dmg):
+	hp = user.get_room_temp('hp', 0)
+	hp -= max(1, dmg - user.rooms_count // 10)
+
+	if hp <= 0:
+		number = databasemanager.get_variable('doctor_num', 1)
+		databasemanager.set_variable('doctor_num', number + 1)
+		user.won(reply)
+	else:
+		user.set_room_temp('hp', hp)
+
 def action(user, reply, text):
 	if text == 'Сдаться':
 		reply('Доктор с ухмылкой сует Лазерную отвертку тебе в нос')
@@ -26,5 +37,3 @@ def action(user, reply, text):
 		user.leave(reply)
 	else:
 		user.fight_action(reply, text)
-		number = databasemanager.get_variable('doctor_num', 1)
-		databasemanager.set_variable('doctor_num', number + 1)

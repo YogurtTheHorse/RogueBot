c56381198331657c2232878ae65474a1ecdd685b
yegorf1 <yegorf1@gmail.com>
2016-09-07 22:15:23 +0300

    Added goal room

---
+++ rooms/usual/goal.py
@@ -0,0 +1,30 @@
+from constants import DICE_MAX
+
+name = 'Мишень'
+
+actions = [ 'Попробовать попасть в нее', 'Уйти' ]
+
+def get_actions(user):
+	return actions
+
+def dice(user, reply, result, subject=None):
+	if result <= DICE_MAX:
+		gold = 10 + result
+		user.gold += gold
+		reply('Держи золото: {0} монет')
+	else:
+		reply('Читатила! Стреляй в него!')
+		user.make_damage(10, 30, reply, name='Кодекс чести лучников')
+	user.leave(reply)
+
+def action(user, reply, text):
+	if text == actions[0]:
+		user.throw_dice(reply)
+	else:
+		reply(
+			'Ты повернулся спиной, а не спене у тебя оказалась _мишень_! Вот почему тебя хотят убить все! '
+			'Ну а еще потому что это страшное подземлье — такие тут правила.\n'
+			'В общем, в твою мишень прилетела стрела.'
+		)
+		user.make_damage(10, 30, reply, name='Какой-то лучник')
+		user.leave(reply)

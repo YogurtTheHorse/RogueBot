d95da5c3b5ab2e8c3e10505a94051a13f5530e20
yegorf1 <yegorf1@gmail.com>
2016-09-09 17:08:20 +0300

    Little bugfix

--- user/missions_defenition.py
+++ user/missions_defenition.py
@@ -1,8 +1,8 @@
 from missions.mission import Mission
 
-def new_mission(mission_name, first_room = 'first'):
+def new_mission(mission_name, path_len=10):
 	for m in self.missions:
 		if m.mission_name == mission_name:
 			return
 
-	self.missions
+	mission = Mission(mission_name, path_len)

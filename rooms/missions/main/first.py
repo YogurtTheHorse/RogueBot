c1e5bdaf1692b1800d3443cac17221e1f446b91d
yegorf1 <yegorf1@gmail.com>
2016-09-10 13:06:35 +0300

    Fixed storyline room

--- rooms/missions/main/first.py
+++ rooms/missions/main/first.py
@@ -7,6 +7,13 @@ def get_actions(user):
 
 def enter(user, reply):
 	reply('_Какой-то сверток..\nПосмотрю потом, как выйду отсюда. Этот старик подозрительно молчит_')
+
+	user.add_item('story', 'gun')
+	user.add_item('story', 'map')
+	user.add_item('neutral', 'bullet')
+	user.add_item('story', 'glasses')
+
+
 	user.new_mission('main', 'second', 10)
 
 def action(user, reply, text):

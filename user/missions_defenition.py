6f0f61ecf9691d6ed0dfea22e8b3368d330c754f
yegorf1 <yegorf1@gmail.com>
2016-09-12 16:18:16 +0300

    Some missions fix

--- user/missions_defenition.py
+++ user/missions_defenition.py
@@ -6,16 +6,10 @@ def new_mission(self, mission_name, room_name='first', path_len=10):
 		if m.name == mission_name:
 			return
 	
-	i = 0
-	while i < len(self.missions):
-		if self.missions[i].path_length == path_len:
-			path_len += 1
-		elif self.missions[i].path_length >= path_len:
-			break
-		i += 1
-
 	mission = Mission(mission_name, room_name, path_len)
-	self.missions.insert(i, mission)
+	self.missions.append(i, mission)
+
+	self.missions = sorted(self.missions, key=mission_sort_key)
 
 def get_last_mission(self):
 	if len(self.missions) > 0:

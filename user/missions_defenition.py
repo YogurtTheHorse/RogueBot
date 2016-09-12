5f5b56bdaec1f8f725d4250f4cfafa6ad8380d76
yegorf1 <yegorf1@gmail.com>
2016-09-12 14:30:08 +0300

    Missions fixed

--- user/missions_defenition.py
+++ user/missions_defenition.py
@@ -5,9 +5,16 @@ def new_mission(self, mission_name, room_name='first', path_len=10):
 	for m in self.missions:
 		if m.name == mission_name:
 			return
+	
+	i = 0
+	while i < len(self.missions):
+		if self.missions[i].path_length == path_len:
+			path_len += 1
+		elif self.missions[i].path_length >= path_len:
+			break
 
 	mission = Mission(mission_name, room_name, path_len)
-	self.missions.add(mission)
+	self.missions.insert(i, mission)
 
 def get_last_mission(self):
 	if len(self.missions) > 0:

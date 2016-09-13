013338b6f3ee5f0eb2f7bc2bd4346ad3bfbb481b
Alex Magdych <sarsncs@gmail.com>
2016-09-13 19:42:56 +0300

    fixed boss loot

--- rooms/boss/black_knight.py
+++ rooms/boss/black_knight.py
@@ -5,7 +5,7 @@ name = 'Черный Рыцарь'
 hp = 129500
 damage_range = ( 0, 129 )
 
-coins = random.randrange( 1000, 10000, 1 )
+coins = random.randrange( 1000, 51000, 1 )
 
 
 def skill_preparing(user, reply, boss):

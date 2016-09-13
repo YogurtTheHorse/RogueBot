47bb9fc45d97aa13bc4844e4a372ec65d5bf260f
Alex Magdych <sarsncs@gmail.com>
2016-09-13 19:44:49 +0300

    fixed boss loot

--- rooms/boss/naping_dragon.py
+++ rooms/boss/naping_dragon.py
@@ -5,7 +5,7 @@ name = 'Разверстый Дракон'
 hp = 77500
 damage_range = ( 0, 77 )
 
-coins = random.randrange( 1000, 50000, 1 )
+coins = random.randrange( 1000, 31000, 1 )
 
 
 def skill_preparing(user, reply, boss):

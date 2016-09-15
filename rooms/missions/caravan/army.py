d6aebdf1298800f727c94bcea08f798243a6c6c9
yegorf1 <yegorf1@gmail.com>
2016-09-15 16:39:47 +0300

    Closed #118

--- rooms/missions/caravan/army.py
+++ rooms/missions/caravan/army.py
@@ -3,24 +3,23 @@ name = 'ОБЪЕДИНЕННАЯ АРМИЯ ПОДЗЕМЕЛЬЯ'
 hp = 15000
 damage_range =  ( 150, 200 )
 
-coins = 13782
+coins = 7782
 
 loot = [  ]
 is_monster = True
+
+def on_won(user, reply):
+	reply('Вы составили мирный договор. Ждите поставок с севера!')
+	user.new_mission('caravan', 'caravan', path_len=75)
+	user.set_room_temp('won', True)
+
+def on_leave(user, reply):
+	if not (user.get_room_temp('won') is True):
+		reply('Мы еще догоним тебя.. Потом')
+		user.new_mission('caravan', 'army', path_len=150)
 
 def enter(user, reply):
 	msg = (
 		'Перед тобой 1337 война. Мне кажется у кого-то большие проблемы.'
 	)
 	reply(msg)
-
-def make_damage(user, reply, dmg):
-	hp = user.get_room_temp('hp', 0)
-	hp -= max(1, dmg - user.rooms_count // 10)
-
-	if hp <= 0:
-		reply('Ты победил, но мы еще вернемся.')
-		user.new_mission('caravan', 'army', path_len=75)
-		user.won(reply)
-	else:
-		user.set_room_temp('hp', hp)

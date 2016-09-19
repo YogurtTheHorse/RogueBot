618827f9069fd7c0fd2aa93c78572cbbe22b1924
yegorf1 <yegorf1@gmail.com>
2016-09-19 16:31:49 +0300

    Resolved #183

---
+++ items/good/swp_pot.py
@@ -0,0 +1,26 @@
+from constants import *
+
+name = 'Черно-белое зелье'
+
+description = (
+	'Пробирка с каким-то черно-белым зельем.'
+)
+
+price = 100
+shop_count = 2
+
+usable = True
+disposable = True
+
+def on_use(user, reply):
+	reply('Это оказалось зелье ПЕРЕМЕН. Ты поменял местами ману и жизнь.')
+
+	user.hp, user.mp = user.mp, user.hp
+	user.heal(1)
+	user.mana(1)
+
+fightable = True
+def fight_use(user, reply, room):
+	on_use(user, reply)
+
+	return 0

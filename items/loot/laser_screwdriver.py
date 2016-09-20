5f09ccc7ee86094c76a1aa3180da49ed0fdb9ba8
yegorf1 <yegorf1@gmail.com>
2016-09-20 21:48:00 +0300

    Doctor nerfed

--- items/loot/laser_screwdriver.py
+++ items/loot/laser_screwdriver.py
@@ -1,8 +1,20 @@
+import databasemanager
+
 name = 'Лазерная отвертка'
 description = '_Бжж-бжж_.'
 price = 1000
 
+defence = 100
+damage = 100
+mana_damage = 100
+
 fightable = True
+
+def on_room(user, reply, room):
+	name = databasemanager.get_variable('doctor_killer')
+	if user.name != name:
+		reply('Кажется не ты настоящий Убийца Доктора.')
+		user.remove_item('laser_screwdriver')
 
 def fight_use(user, reply, room):
 	reply('*Бжж-бжж*.')

0f058ae01a8040e5e12297c2ed308c10c721665d
Encamy <alexxey67@gmail.com>
2016-09-23 00:07:59 +0400

    Delete check

--- items/bad/scroll_stone.py
+++ items/bad/scroll_stone.py
@@ -1,5 +1,4 @@
 from constants import *
-import random
 
 name = 'Свиток вызова камней'
 
@@ -16,8 +15,6 @@ def fight_use(user, reply, room):
 	if user.use_mana(50):
 		reply('С неба падают камни прямо на тебя и наносят 50 урона.')
 		user.make_damage(50, 100, reply, death=True, name='Камнепад')
-		if user.hp <= 0:
-			user.death(reply, reason='Камнепад')
 	else:
 		reply('Недостаточно маны')
 		user.add_item('bad', 'scroll_stone')

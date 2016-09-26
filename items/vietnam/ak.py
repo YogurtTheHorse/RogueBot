616cc9f0026e06ac905ab5894922292ee62df6f1
daemonica <dima62131@aol.com>
2016-09-27 01:26:34 +0300

    Vietnam DLC

---
+++ items/vietnam/ak.py
@@ -0,0 +1,17 @@
+name = 'АК-47'
+description = (
+    'Исполняет русский рэп, пока лежит в инвентаре'
+)
+
+price = 300
+
+fightable = True
+
+strengthoff = True
+
+def fight_use(user, reply, room):
+	res = 0
+	while user.has_item('bullet') and res // 60 < 10:
+		user.remove_item('bullet')
+		res += 60
+	return res

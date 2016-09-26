9df58c5bea17341c8b342a4c78a712e2f02a66c9
daemonica <dima62131@aol.com>
2016-09-27 01:28:40 +0300

    Vietnam DLC

---
+++ items/vietnam/m79.py
@@ -0,0 +1,17 @@
+name = 'M79'
+description = (
+    'Уничтожает. Быстро.'
+)
+
+price = 800
+
+fightable = True
+
+strengthoff = True
+
+def fight_use(user, reply, room):
+	res = 400
+	while user.has_item('bullet') and res // 20 < 10:
+		user.remove_item('bullet')
+		res += 20
+	return res

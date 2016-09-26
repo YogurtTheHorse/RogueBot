0a1057ae6884102bacc6a7b57e1c98b526442e99
daemonica <dima62131@aol.com>
2016-09-27 01:22:38 +0300

    Vietnam DLC

---
+++ rooms/monster/vietnam/bush_soldier.py
@@ -0,0 +1,11 @@
+name = 'Куст'
+hp = 280
+damage_range = (20, 35)
+
+coins = 200
+
+loot = [ 'm-16' ]
+
+def enter(user, reply):
+    reply('Когда ты проходил мимо куста, из него выпрыгнул солдат и ударил тебя прикладом')
+    user.make_damage(0, 30, reply, name='Приклад')

c01ac260067f7e5b8ff3064477c4b823f6e767da
yegorf1 <yegorf1@gmail.com>
2016-10-02 17:00:02 +0300

    some fix

--- user/levels_defenition.py
+++ user/levels_defenition.py
@@ -1,5 +1,5 @@
 def prepare_boss(self):
-	next_level = self.get_next_level()
+	next_level = self.get_next_level(True)
 
 	if next_level not in self.levels and next_level is not None:
 		self.levels.append(next_level)
@@ -12,14 +12,14 @@ def get_prev_level(self):
 	else:
 		return None
 
-def get_next_level(self):
+def get_next_level(self, force=False):
 	res = None
 	if self.level == 'easy':
 		res = 'medium'
 	elif self.level == 'medium':
 		res = 'hard'
 
-	if res is not None and res in self.levels:
+	if res is not None and (res in self.levels or force):
 		return res
 	else:
 		return None

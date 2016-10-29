e37e96926981fba6a24c1f1aaf58012e489f01eb
yegorf1 <yegorf1@gmail.com>
2016-10-29 15:53:46 +0300

    Fixed #264

--- rooms/default/monster/easy/sylph.py
+++ rooms/default/monster/easy/sylph.py
@@ -9,4 +9,4 @@ loot = ['ballet_tutu']  # Балетная пачка
 
 
 def enter(user, reply):
-	reply('Не доверяй этой женшине, ой, не доверяй!', photo='BQADAgADDAkAAmrZzgdS6xec3MqmYQI')
+	reply('Не доверяй этой женщине, ой, не доверяй!', photo='BQADAgADDAkAAmrZzgdS6xec3MqmYQI')

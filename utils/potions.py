e99ae6f4d96e5bda6fb7b98ce342c48785742b34
yegorf1 <yegorf1@gmail.com>
2016-12-07 15:47:26 +0300

    Fixed potions

--- utils/potions.py
+++ utils/potions.py
@@ -11,17 +11,17 @@ DALTONISM_POT = 6
 def get_potion_color(user, pot_num):
 	if user is None:
 		return 'Gray'
- 
-	colors = [ 
-		locale_manager.get('gray', user.lang),
-		locale_manager.get('black', user.lang),
-		locale_manager.get('white', user.lang),
-		locale_manager.get('yellow', user.lang),
-		locale_manager.get('rainbow', user.lang),
-		locale_manager.get('brown', user.lang),
-		locale_manager.get('red', user.lang),
-		locale_manager.get('pink', user.lang),
-		locale_manager.get('azure', user.lang)
+
+	colors = [
+		locale_manager.get('items.potions.gray', user.lang),
+		locale_manager.get('items.potions.black', user.lang),
+		locale_manager.get('items.potions.white', user.lang),
+		locale_manager.get('items.potions.yellow', user.lang),
+		locale_manager.get('items.potions.rainbow', user.lang),
+		locale_manager.get('items.potions.brown', user.lang),
+		locale_manager.get('items.potions.red', user.lang),
+		locale_manager.get('items.potions.pink', user.lang),
+		locale_manager.get('items.potions.azure', user.lang)
 	]
 
 	if user.has_tag('daltonism') or pot_num >= len(colors):

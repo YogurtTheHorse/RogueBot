2210cf08765c95f6717c10ea6ae45f7c9c04f1c1
Molodetskiy Arseniy <molodeckij.b.2017@fml31.ru>
2018-03-22 00:42:07 +0300

    Feature/alice (#288)
    * added main_alice.py file
    
    * fixed assignment in lambda
    
    * fixed uid error
    
    * fixed missing variable named like COMMAND
    
    * fixed wrong misspelling
    
    * removed unused attribute
    
    * added global variable
    
    * initializing new user
    
    * added buttons view
    
    * added buttons view

---
+++ main_alice.py
@@ -0,0 +1,83 @@
+# coding utf-8
+# Импортирует поддержку UTF-8.
+from __future__ import unicode_literals
+
+# Импортируем модули для работы с JSON и логами.
+import json
+import logging
+import usermanager
+import config
+
+# Импортируем подмодули Flask для запуска веб-сервиса.
+from flask import Flask, request
+app = Flask(__name__)
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+# Хранилище данных о сессиях.
+sessionStorage = {}
+
+# Задаем параметры приложения Flask.
+@app.route("/", methods=['POST'])
+def main():
+# Функция получает тело запроса и возвращает ответ.
+    logging.info('Request %r', request.json)
+
+    response = {
+        "version": request.json['version'],
+        "session": request.json['session'],
+        "response": {
+            "end_session": False
+        }
+    }
+
+    handle_dialog(request.json, response)
+
+    logging.info('Response %r', response)
+
+    return json.dumps(
+        response,
+        ensure_ascii=False,
+        indent=2
+    )
+
+# Функция для непосредственной обработки диалога.
+def handle_dialog(req, res):
+    user_id = 'alice' + req['session']['user_id']
+    
+    if req['session']['new']:
+        # Это новый пользователь.
+        # Инициализируем сессию и поприветствуем его.
+        res['response']['text'] = 'Теперь скажи мне свое имя'
+        usermanager.new_user(user_id)
+        return
+    global text, buttons_list
+    text = ''
+    buttons_list = [ ]
+
+    def reply(txt, buttons=None, photo=None):
+        global text, buttons_list
+        text = text + '\n\n' + txt
+
+        if buttons is None:
+            return
+
+        for btn in buttons:
+            if isinstance(btn, list):
+                buttons_list.extend(btn)
+            else:
+                buttons_list.append(btn)
+
+    usermanager.message(user_id, reply, req['request']['command'])
+    
+    res['response']['text'] = text.strip()
+
+    res['response']['buttons'] = [
+        {'title': btn, 'hide': False}
+        for btn in buttons_list
+    ]
+
+
+
+
